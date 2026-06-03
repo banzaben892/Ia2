b0845333ff24fe8a4342289508f2b4f23e77db0a
banzaben892 <banzaben892@gmail.com>
2026-06-03 22:48:33 +0200

    Update storage.js

--- data/storage.js
+++ data/storage.js
@@ -1,7 +1,5 @@
 const Storage = {
-  save(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
-  },
+  save(key, value) { localStorage.setItem(key, JSON.stringify(value)); },
   load(key, defaultValue = null) {
     const data = localStorage.getItem(key);
     return data ? JSON.parse(data) : defaultValue;
