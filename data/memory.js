70b2c8e60d85ccdc86a85c063c7a950a5a37e5e9
banzaben892 <banzaben892@gmail.com>
2026-06-03 22:47:54 +0200

    Refactor Memory class methods to single-line format
    Condensed methods in the Memory class into single-line definitions.

--- data/memory.js
+++ data/memory.js
@@ -3,15 +3,7 @@ class Memory {
     this.userName = Storage.load('userName', '');
     this.context = Storage.load('context', {});
   }
-  setUserName(name) {
-    this.userName = name;
-    Storage.save('userName', name);
-  }
-  updateContext(key, value) {
-    this.context[key] = value;
-    Storage.save('context', this.context);
-  }
-  getContext(key) {
-    return this.context[key];
-  }
+  setUserName(name) { this.userName = name; Storage.save('userName', name); }
+  updateContext(key, value) { this.context[key] = value; Storage.save('context', this.context); }
+  getContext(key) { return this.context[key]; }
 }
