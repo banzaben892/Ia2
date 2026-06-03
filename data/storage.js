fbf2fef2c82443ceaecf01690d132b5cf6fb5880
banzaben892 <banzaben892@gmail.com>
2026-06-03 19:49:25 +0200

    Create storage.js

---
+++ data/storage.js
@@ -0,0 +1,9 @@
+const Storage = {
+  save(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  load(key, defaultValue = null) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : defaultValue;
+  }
+};
